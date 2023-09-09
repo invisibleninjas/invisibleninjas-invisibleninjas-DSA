7c014cdc82dbfb7b0b088604ab142c9fece2f935
invisibleninjas <134318976+invisibleninjas@users.noreply.github.com>
2023-09-10 01:48:16 +0530

    Time: 0 ms (100%), Space: 39.8 MB (49.15%) - LeetHub

---
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int indexSum = 0, sum = 0, chunkCount = 0;
+        for(int i =0; i<arr.length; i++){
+            indexSum = indexSum + i;
+            sum = sum + arr[i];
+            if(sum == indexSum){
+                chunkCount++;
+            }
+        }
+        return chunkCount;
+        
+    }
+}
